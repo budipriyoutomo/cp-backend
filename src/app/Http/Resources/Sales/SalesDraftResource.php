a50c2f8f6d0bc7ed4ebbca69f6f6f6b157231405
Budi Priyo Utomo <utomo2@gmail.com>
2026-05-11 16:44:12 +0700

    update salesdraft resource

---
+++ src/app/Http/Resources/Sales/SalesDraftResource.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Resources\Sales;
+
+use App\Http\Resources\BaseResource;
+
+class SalesDraftResource extends BaseResource
+{
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'date' => optional($this->date)->format('Y-m-d'),
+
+            'outlet_name' => optional($this->outlet)->name,
+
+            'items' => $this->whenLoaded('items', function () {
+                return $this->items->map(function ($item) {
+
+                    return [
+                        'platecolor' => optional($item->platecolor)->platename,
+
+                        'price' => (float) optional($item->platecolor)->price,
+
+                        'pos' => (int) ($item->pos ?? 0),
+                        'sold' => (int) ($item->sold ?? 0),
+                        'production' => (int) ($item->production ?? 0),
+                        'waste' => (int) ($item->waste ?? 0),
+                        'adjustment' => (int) ($item->adjustment ?? 0),
+                        'compensation' => (int) ($item->compensation ?? 0),
+                        'selisih' => (int) ($item->selisih ?? 0),
+                    ];
+                });
+            }),
+
+            ...$this->systemFields(),
+        ];
+    }
+}

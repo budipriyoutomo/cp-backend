7b2cd3151b06dbe6a428743cb9e1ead2286f9db8
Budi Priyo Utomo <utomo2@gmail.com>
2026-05-13 11:37:16 +0700

    update cors

--- src/config/cors.php
+++ src/config/cors.php
@@ -23,6 +23,7 @@ return [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
         'https://*.vercel.app',
+        'https://*.calira.my.id'
     ],
 
     'allowed_origins_patterns' => ['#^https://.*\.vusercontent\.net$#',],
